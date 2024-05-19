01980f81e4bbbb1c713283fdcc56539346f7acfb
Md. Shakil Rana <159621858+MDshakilr@users.noreply.github.com>
2024-05-19 08:08:25 +0600

    Update 1.cpp

--- oop in c++/basic/1.cpp
+++ oop in c++/basic/1.cpp
@@ -9,3 +9,34 @@ x=y--;
 cout<<x<<endl;
 cout<<y<<endl;
 }
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+class ScholarHat {
+private:
+ int y; // Private attribute
+public:
+ int x; // Public attribute
+
+ void display(int a) { // Public method
+ y = a;
+ cout << "y = " << y << ", x = " << x << endl;
+ }
+};
+
+int main() {
+ ScholarHat myObj;
+ myObj.x = 70;
+ myObj.display(80);
+ return 0;
+}
